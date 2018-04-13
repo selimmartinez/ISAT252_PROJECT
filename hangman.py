740399bf811f50fc680869c9a1d30561e4fc4173
selimmartinez <36453094+selimmartinez@users.noreply.github.com>
2018-04-13 09:55:16 -0400

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,13 @@
+#hangman.py
+
+import random
+
+print("Welcome to Hangman!" 
+		"\nWe will be generating random words for you to guess."
+		"\nYou will guess a letter and will have up to 6 lives" 
+		"\nor else little Jimmy will be doomed. GOOD LUCK!")
+
+WORDS = ("hello c", "pythonian", "javascripter", "boolean", "hello world", 
+		"code reader", "socializer", "commando", "caffeinator", "sublime")
+word = random.choice(WORDS)
+
--- hangman_test.py
+++ hangman_test.py
@@ -1,7 +1,6 @@
 #hangman_test.py
+import random 
 
-print ("Welcome to Hangman! \nWe will be generating random words for you to guess. \nYou will guess a letter and will have up to 6 lives \nor else little Jimmy will be doomed. GOOD LUCK!")
-
-WORDS("hello c", "pythonian", "javascripter", "boolean", "hello world", "code reader", "socializer", "commando", "caffeinator", "sublime")
-word = random.choice(WORDS)
+def test_smoke():
+	
 
